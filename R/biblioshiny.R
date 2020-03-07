0038656bd8a3360eac9a0e18e9a721848a5e016e
massimoaria <aria@unina.it>
2020-03-07 13:09:22 +0100

    Help recompiled using new roxygen version

--- R/biblioshiny.R
+++ R/biblioshiny.R
@@ -2,6 +2,14 @@
 #'
 #' \code{biblioshiny} performs science mapping analysis using the main functions of the bibliometrix package.
 #'
+#' @param port is the TCP port that the application should listen on. If the port is not specified, 
+#' and the shiny.port option is set (with options(shiny.port = XX)), then that port will be used. 
+#' Otherwise, use a random port.
+#' 
+#' @param launch.browser If true, the system's default web browser will be launched automatically 
+#' after the app is started. Defaults to true in interactive sessions only. This value of 
+#' this parameter can also be a function to call with the application's URL.
+#'  
 #' @examples
 #' 
 #' #biblioshiny()
@@ -9,7 +17,7 @@
 #' 
 #' @export
 
-biblioshiny <- function(){
+biblioshiny <- function(port = NULL, launch.browser=TRUE){
   
-  runApp(system.file("biblioshiny",package="bibliometrix"),launch.browser = TRUE)
+  runApp(system.file("biblioshiny",package="bibliometrix"),launch.browser = launch.browser, port = port)
 }

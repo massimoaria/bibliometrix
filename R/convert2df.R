8d40c08b42af102ec3b1f298d5bfac8f801c6229
massimoaria <aria@unina.it>
2021-01-18 12:47:02 +0100

    Issue #162: Solved

--- R/convert2df.R
+++ R/convert2df.R
@@ -145,7 +145,12 @@ convert2df<-function(file,dbsource="wos",format="plaintext"){
     M$TC[is.na(M$TC)] <- 0
   } else {M$TC <- 0}
   
-  if (!("CR" %in% names(M))){M$CR="none"}
+  if (!("CR" %in% names(M))){
+    M$CR="none"
+  } else {
+    M$CR <- trim.leading(trimES(gsub("\\[,||\\[||\\]","",M$CR)))  # remove foreign characters from CR (i.e. Chinese, Russian characters)
+  }
+  
   if (dbsource!="cochrane"){M$AU=gsub(intToUtf8(8217),intToUtf8(39),M$AU)}
   
   cat("Done!\n\n")

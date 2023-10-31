ed91e654d79d9cc3576d356382015fc531abe087
massimoaria <aria@unina.it>
2023-10-31 17:02:10 +0100

    minor fix

--- R/missingData.R
+++ R/missingData.R
@@ -30,7 +30,7 @@ missingData <- function(M) {
     sum(is.na(M[,x]) | M[,x] %in% c("NA,0000,NA","NA","","none"))
     })
   
-  if (sum(M$TC, na.rm=T)==0){missing_counts["TC"]=nrow(M)}
+  if (sum(as.numeric(M$TC), na.rm=T)==0){missing_counts["TC"]=nrow(M)}
   # calculate the percentage of missing values in each column
   missing_pct <- round(missing_counts/nrow(M) * 100, 2)
   # create a dataframe with the column names, number of missing values and percentage of missing values

9b7b7685323c9ecf043c227de21b6fb04ff740d3
Massimo Aria <aria@unina.it>
2024-04-29 14:38:07 +0200

    Add helpContent() function

---
+++ inst/biblioshiny/helpContent.R
@@ -0,0 +1,161 @@
+helpContent <- function(){
+  info <- "<body lang=IT link='#467886' vlink='#96607D' style='tab-interval:35.4pt;
+word-wrap:break-word'>
+  
+  <div class=WordSection1>
+    
+    <p class=MsoNormal align=center style='text-align:center'><b><span lang=EN-US
+    style='font-size:16.0pt;font-family:'Times New Roman',serif;color:black;
+background:white;mso-ansi-language:EN-US'>Supported Bibliographic Databases and
+    Suggested File Formats<o:p></o:p></span></b></p>
+      
+      <p class=MsoNormal><b><span lang=EN-US style='font-size:12.0pt;font-family:
+'Times New Roman',serif;color:black;background:white;mso-ansi-language:EN-US'><o:p>&nbsp;</o:p></span></b></p>
+      
+      <p class=MsoNormal><b><span lang=EN-US style='font-size:12.0pt;font-family:
+'Times New Roman',serif;color:black;background:white;mso-ansi-language:EN-US'><o:p>&nbsp;</o:p></span></b></p>
+      
+      <p class=MsoNormal><b><span lang=EN-US style='font-size:12.0pt;font-family:
+'Times New Roman',serif;color:black;background:white;mso-ansi-language:EN-US'>Biblioshiny</span></b><span
+    lang=EN-US style='font-size:12.0pt;font-family:'Times New Roman',serif;
+color:black;background:white;mso-ansi-language:EN-US'> imports and analyzes
+    collection exported from:<o:p></o:p></span></p>
+      
+      <p class=MsoListParagraphCxSpFirst style='text-indent:-18.0pt;mso-list:l0 level1 lfo1'><![if !supportLists]><span
+    lang=EN-US style='font-size:12.0pt;font-family:Symbol;mso-fareast-font-family:
+Symbol;mso-bidi-font-family:Symbol;color:black;mso-ansi-language:EN-US'><span
+    style='mso-list:Ignore'>·<span style='font:7.0pt 'Times New Roman''>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    </span></span></span><![endif]><em><span lang=EN-US style='font-size:12.0pt;
+color:black;background:white;mso-ansi-language:EN-US'>Web of Science </span></em><em><span
+    lang=EN-US style='font-size:12.0pt;color:black;background:white;mso-ansi-language:
+EN-US;font-style:normal'>(</span></em><span style='font-size:12.0pt;font-family:
+'Times New Roman',serif'><a href='http://www.webofscience.com'><span
+                          lang=EN-US style='background:white;mso-ansi-language:EN-US'>www.webofscience.com</span></a></span><em><span
+                          lang=EN-US style='font-size:12.0pt;color:black;background:white;mso-ansi-language:
+EN-US;font-style:normal'>)</span></em><span lang=EN-US style='font-size:12.0pt;
+font-family:'Times New Roman',serif;color:black;background:white;mso-ansi-language:
+EN-US'><o:p></o:p></span></p>
+      
+      <p class=MsoListParagraphCxSpMiddle style='text-indent:-18.0pt;mso-list:l0 level1 lfo1'><![if !supportLists]><span
+    lang=EN-US style='font-size:12.0pt;font-family:Symbol;mso-fareast-font-family:
+Symbol;mso-bidi-font-family:Symbol;color:black;mso-ansi-language:EN-US'><span
+    style='mso-list:Ignore'>·<span style='font:7.0pt 'Times New Roman''>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    </span></span></span><![endif]><em><span lang=EN-US style='font-size:12.0pt;
+color:black;background:white;mso-ansi-language:EN-US'>Scopus</span></em><em><span
+    lang=EN-US style='font-size:12.0pt;color:black;background:white;mso-ansi-language:
+EN-US;font-style:normal'> (</span></em><span style='font-size:12.0pt;
+font-family:'Times New Roman',serif'><a href='http://www.scopus.com'><span
+                           lang=EN-US style='background:white;mso-ansi-language:EN-US'>www.scopus.com</span></a></span><em><span
+                           lang=EN-US style='font-size:12.0pt;color:black;background:white;mso-ansi-language:
+EN-US;font-style:normal'>) </span></em><span lang=EN-US style='font-size:12.0pt;
+font-family:'Times New Roman',serif;color:black;background:white;mso-ansi-language:
+EN-US'><o:p></o:p></span></p>
+      
+      <p class=MsoListParagraphCxSpMiddle style='text-indent:-18.0pt;mso-list:l0 level1 lfo1'><![if !supportLists]><span
+    lang=EN-US style='font-size:12.0pt;font-family:Symbol;mso-fareast-font-family:
+Symbol;mso-bidi-font-family:Symbol;color:black;mso-ansi-language:EN-US'><span
+    style='mso-list:Ignore'>·<span style='font:7.0pt 'Times New Roman''>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    </span></span></span><![endif]><i><span lang=EN-US style='font-size:12.0pt;
+font-family:'Times New Roman',serif;color:black;background:white;mso-ansi-language:
+EN-US'>OpenAlex</span></i><span lang=EN-US style='font-size:12.0pt;font-family:
+'Times New Roman',serif;color:black;background:white;mso-ansi-language:EN-US'> (</span><span
+                                                                                style='font-size:12.0pt;font-family:'Times New Roman',serif'><a
+                                                                                href='http://www.openalex.org'><span lang=EN-US style='background:white;
+mso-ansi-language:EN-US'>www.openalex.org</span></a></span><span lang=EN-US
+                                                                                style='font-size:12.0pt;font-family:'Times New Roman',serif;color:black;
+background:white;mso-ansi-language:EN-US'>) <o:p></o:p></span></p>
+      
+      <p class=MsoListParagraphCxSpMiddle style='text-indent:-18.0pt;mso-list:l0 level1 lfo1'><![if !supportLists]><span
+    lang=EN-US style='font-size:12.0pt;font-family:Symbol;mso-fareast-font-family:
+Symbol;mso-bidi-font-family:Symbol;color:black;mso-ansi-language:EN-US'><span
+    style='mso-list:Ignore'>·<span style='font:7.0pt 'Times New Roman''>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    </span></span></span><![endif]><em><span lang=EN-US style='font-size:12.0pt;
+color:black;background:white;mso-ansi-language:EN-US'>Dimensions </span></em><em><span
+    lang=EN-US style='font-size:12.0pt;color:black;background:white;mso-ansi-language:
+EN-US;font-style:normal'>(</span></em><span style='font-size:12.0pt;font-family:
+'Times New Roman',serif'><a href='http://www.dimensions.ai'><span lang=EN-US
+                          style='background:white;mso-ansi-language:EN-US'>www.dimensions.ai</span></a></span><em><span
+                          lang=EN-US style='font-size:12.0pt;color:black;background:white;mso-ansi-language:
+EN-US;font-style:normal'>) </span></em><span lang=EN-US style='font-size:12.0pt;
+font-family:'Times New Roman',serif;color:black;background:white;mso-ansi-language:
+EN-US'><o:p></o:p></span></p>
+      
+      <p class=MsoListParagraphCxSpMiddle style='text-indent:-18.0pt;mso-list:l0 level1 lfo1'><![if !supportLists]><span
+    lang=EN-US style='font-size:12.0pt;font-family:Symbol;mso-fareast-font-family:
+Symbol;mso-bidi-font-family:Symbol;color:black;mso-ansi-language:EN-US'><span
+    style='mso-list:Ignore'>·<span style='font:7.0pt 'Times New Roman''>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    </span></span></span><![endif]><em><span lang=EN-US style='font-size:12.0pt;
+color:black;background:white;mso-ansi-language:EN-US'>The Lens </span></em><em><span
+    lang=EN-US style='font-size:12.0pt;color:black;background:white;mso-ansi-language:
+EN-US;font-style:normal'>(</span></em><span style='font-size:12.0pt;font-family:
+'Times New Roman',serif'><a href='http://www.lens.org'><span lang=EN-US
+                          style='background:white;mso-ansi-language:EN-US'>www.lens.org</span></a></span><em><span
+                          lang=EN-US style='font-size:12.0pt;color:black;background:white;mso-ansi-language:
+EN-US;font-style:normal'>) </span></em><span lang=EN-US style='font-size:12.0pt;
+font-family:'Times New Roman',serif;color:black;background:white;mso-ansi-language:
+EN-US'><o:p></o:p></span></p>
+      
+      <p class=MsoListParagraphCxSpMiddle style='text-indent:-18.0pt;mso-list:l0 level1 lfo1'><![if !supportLists]><span
+    lang=EN-US style='font-size:12.0pt;font-family:Symbol;mso-fareast-font-family:
+Symbol;mso-bidi-font-family:Symbol;color:black;mso-ansi-language:EN-US'><span
+    style='mso-list:Ignore'>·<span style='font:7.0pt 'Times New Roman''>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    </span></span></span><![endif]><em><span lang=EN-US style='font-size:12.0pt;
+color:black;background:white;mso-ansi-language:EN-US'>PubMed</span></em><em><span
+    lang=EN-US style='font-size:12.0pt;color:black;background:white;mso-ansi-language:
+EN-US;font-style:normal'> (</span></em><span style='font-size:12.0pt;
+font-family:'Times New Roman',serif'><a href='https://pubmed.ncbi.nlm.nih.gov/'><span
+                           lang=EN-US style='background:white;mso-ansi-language:EN-US'>https://pubmed.ncbi.nlm.nih.gov/</span></a></span><em><span
+                           lang=EN-US style='font-size:12.0pt;color:black;background:white;mso-ansi-language:
+EN-US;font-style:normal'>) </span></em><span lang=EN-US style='font-size:12.0pt;
+font-family:'Times New Roman',serif;color:black;background:white;mso-ansi-language:
+EN-US'><o:p></o:p></span></p>
+      
+      <p class=MsoListParagraphCxSpLast style='text-indent:-18.0pt;mso-list:l0 level1 lfo1'><![if !supportLists]><span
+    lang=EN-US style='font-size:12.0pt;font-family:Symbol;mso-fareast-font-family:
+Symbol;mso-bidi-font-family:Symbol;color:black;mso-ansi-language:EN-US'><span
+    style='mso-list:Ignore'>·<span style='font:7.0pt 'Times New Roman''>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    </span></span></span><![endif]><em><span lang=EN-US style='font-size:12.0pt;
+color:black;background:white;mso-ansi-language:EN-US'>Cochrane Library</span></em><span
+    lang=EN-US style='font-size:12.0pt;font-family:'Times New Roman',serif;
+color:black;background:white;mso-ansi-language:EN-US'> (</span><span
+                                                        style='font-size:12.0pt;font-family:'Times New Roman',serif'><a
+                                                        href='http://www.cochranelibrary.com/'><span lang=EN-US style='background:white;
+mso-ansi-language:EN-US'>www.cochranelibrary.com/</span></a></span><span
+                                                        lang=EN-US style='font-size:12.0pt;font-family:'Times New Roman',serif;
+color:black;background:white;mso-ansi-language:EN-US'>) <o:p></o:p></span></p>
+      
+      <p class=MsoNormal><span lang=EN-US style='font-size:12.0pt;font-family:'Times New Roman',serif;
+color:black;background:white;mso-ansi-language:EN-US'><o:p>&nbsp;</o:p></span></p>
+      
+      <p class=MsoNormal><span lang=EN-US style='font-size:12.0pt;font-family:'Times New Roman',serif;
+color:black;background:white;mso-ansi-language:EN-US'><o:p>&nbsp;</o:p></span></p>
+      
+      <p style='margin-top:6.0pt;margin-right:0cm;margin-bottom:6.0pt;margin-left:
+0cm;background:white'><i><span lang=EN-US style='font-size:12.0pt;color:black;
+mso-ansi-language:EN-US'>Web of Science</span></i><span lang=EN-US
+    style='font-size:12.0pt;color:black;mso-ansi-language:EN-US'>, <i>Scopus</i>
+      and <i>OpenAlex</i> allow the user to export the complete set of metadata. This
+    means that it will be possible to perform all analyses implemented in biblioshiny.
+    <o:p></o:p></span></p>
+      
+      <p style='margin-top:6.0pt;margin-right:0cm;margin-bottom:6.0pt;margin-left:
+0cm;background:white'><span lang=EN-US style='font-size:12.0pt;color:black;
+mso-ansi-language:EN-US'>Some other databases, such as Dimensions, PubMed and
+    Cochrane Library, export just a limited set of metadata types which implies
+    some limitations in the choice of the analyses to carry out.<o:p></o:p></span></p>
+      
+      <p style='margin-top:6.0pt;margin-right:0cm;margin-bottom:6.0pt;margin-left:
+0cm;background:white'><span lang=EN-US style='font-size:12.0pt;color:black;
+mso-ansi-language:EN-US'>In the following table, for each database we repot the
+    file formats supported and the metadata contained in each. Finally, the
+    suggested file format is reported.<o:p></o:p></span></p>
+      
+      <p class=MsoNormal><span lang=EN-US style='font-size:12.0pt;font-family:'Times New Roman',serif;
+mso-ansi-language:EN-US'><o:p>&nbsp;</o:p></span></p>
+      
+      </div>
+      
+      </body>"
+  
+  return(list(info=info))
+}

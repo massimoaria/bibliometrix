94b925caeb650f12c0f56c58e1a43e3f138aa41c
Massimo Aria <16023543+massimoaria@users.noreply.github.com>
2025-11-04 22:44:35 +0100

    Minor fixes

--- R/plotThematicEvolution.R
+++ R/plotThematicEvolution.R
@@ -1,3 +1,16 @@
+utils::globalVariables(c(
+  "CL1",
+  "CL2",
+  "color_assigned",
+  "color_final",
+  "color_from",
+  "color_lineage",
+  "color_to",
+  "measure",
+  "total_in",
+  "weight"
+))
+
 #' Plot a Thematic Evolution Analysis
 #'
 #' It plot a Thematic Evolution Analysis performed using the \code{\link{thematicEvolution}} function.
@@ -21,7 +34,7 @@
 #' }
 #'
 #' @seealso \code{\link{thematicMap}} function to create a thematic map based on co-word network analysis and clustering.
-#' @seealso \code{\link{thematicMap}} function to perform a thematic evolution analysis.
+#' @seealso \code{\link{thematicEvolution}} function to perform a thematic evolution analysis.
 #' @seealso \code{\link{networkPlot}} to plot a bibliographic network.
 #'
 #' @export
@@ -217,11 +230,42 @@ assignEvolutionColors <- function(
 
   # Initialize palette
   if (is.null(palette)) {
-    library(RColorBrewer)
     palette <- c(
-      brewer.pal(8, "Set2"),
-      brewer.pal(8, "Dark2"),
-      brewer.pal(8, "Accent")
+      "#E41A1C",
+      "#377EB8",
+      "#4DAF4A",
+      "#984EA3",
+      "#FF7F00",
+      "#A65628",
+      "#F781BF",
+      "#999999",
+      "#66C2A5",
+      "#FC8D62",
+      "#8DA0CB",
+      "#E78AC3",
+      "#A6D854",
+      "#FFD92F",
+      "#B3B3B3",
+      "#A6CEE3",
+      "#1F78B4",
+      "#B2DF8A",
+      "#33A02C",
+      "#FB9A99",
+      "#E31A1C",
+      "#FDBF6F",
+      "#FF7F00",
+      "#CAB2D6",
+      "#6A3D9A",
+      "#B15928",
+      "#8DD3C7",
+      "#BEBADA",
+      "#FB8072",
+      "#80B1D3",
+      "#FDB462",
+      "#B3DE69",
+      "#D9D9D9",
+      "#BC80BD",
+      "#CCEBC5"
     )
   }
 
@@ -367,105 +411,3 @@ assignEvolutionColors <- function(
 
   return(list(Nodes = Nodes, Edges = Edges))
 }
-
-# plotThematicEvolution <- function(
-#   Nodes,
-#   Edges,
-#   measure = "inclusion",
-#   min.flow = 0
-# ) {
-#   Kx <- length(unique(Nodes$group))
-#   Ky <- nrow(Nodes)
-#   Nodes <- Nodes %>%
-#     mutate(
-#       coordX = rep(
-#         seq(from = 0, to = 1, by = 1 / (Kx - 0.8)),
-#         as.numeric(table(.data$group))
-#       ),
-#       coordY = rep(0.1, Ky)
-#     )
-#
-#   ################
-#   switch(
-#     measure,
-#     inclusion = {
-#       Edges = Edges[-c(4, 5)]
-#     },
-#     stability = {
-#       Edges = Edges[-c(3, 4)]
-#     },
-#     weighted = {
-#       Edges = Edges[, -c(3, 5)]
-#     }
-#   )
-#   names(Edges)[3] = "weight"
-#   Edges = Edges[Edges$weight >= min.flow, ]
-#   Edges$weight = Edges$weight * 100
-#
-#   Nodes$id <- (1:nrow(Nodes)) - 1
-#
-#   ## identify and remove nodes with empty edges
-#   ind <- setdiff(Nodes$id, unique(c(Edges$from, Edges$to)))
-#   if (length(ind) > 0) {
-#     Nodes <- Nodes[-(ind + 1), ]
-#     Nodes$idnew <- (1:nrow(Nodes)) - 1
-#     ## replace old edge ids with new ones
-#     for (i in 1:nrow(Nodes)) {
-#       old <- Nodes$id[i]
-#       new <- Nodes$idnew[i]
-#       Edges$from[Edges$from == old] <- new
-#       Edges$to[Edges$to == old] <- new
-#     }
-#   }
-#
-#   #Edges$color <- "lightgrey"
-#
-#   # plotly margins
-#   m <- list(
-#     l = 50,
-#     r = 50,
-#     b = 100,
-#     t = 100,
-#     pad = 4
-#   )
-#
-#   plotly::plot_ly(
-#     type = "sankey",
-#     arrangement = "snap",
-#     node = list(
-#       label = Nodes$name,
-#       x = Nodes$coordX,
-#       y = Nodes$coordY,
-#       color = Nodes$color,
-#       pad = 4
-#     ), # 10 Pixel
-#     link = list(
-#       source = Edges$from,
-#       target = Edges$to,
-#       value = Edges$weight
-#       #,color = Edges$color
-#     )
-#   ) %>%
-#     layout(margin = m) %>%
-#     plotly::add_annotations(
-#       x = Nodes$coordX,
-#       y = 1.08,
-#       text = factor(Nodes$group),
-#       showarrow = F,
-#       xanchor = "center",
-#       font = list(color = 'Dark', family = "TimesNewRoman", size = 18)
-#     ) %>%
-#     config(
-#       displaylogo = FALSE,
-#       modeBarButtonsToRemove = c(
-#         'toImage',
-#         'sendDataToCloud',
-#         'pan2d',
-#         'select2d',
-#         'lasso2d',
-#         'toggleSpikelines',
-#         'hoverClosestCartesian',
-#         'hoverCompareCartesian'
-#       )
-#     )
-# }
